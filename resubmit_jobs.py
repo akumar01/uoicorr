d145f5a729395ba84b992d4165aa6776a6c01762
akumar01 <akumar.4db0@gmail.com>
2019-01-18 18:52:53 -0800

    Added functionality to resubmit_jobs.py

--- resubmit_jobs.py
+++ resubmit_jobs.py
@@ -2,15 +2,80 @@ import sys, os
 import argparse
 import glob
 import pdb
+import json
+
 
 # Resubmit all the jobs in a given directory or a file
-parser = argparse.ArgumentParser()
-parser.add_argument('directory', default = None)
-a = parser.parse_args()
-
-if a.directory: 
-	# Collect all .h5 files
-	sbatch_files = glob.glob('uoicorr/%s/sbatch*' % a.directory)
+def resubmit_jobs(directory, jobnums = None):
+	# Collect all sbatch files
+	sbatch_files = glob.glob('uoicorr/%s/sbatch*' % directory)
 	for sbf in sbatch_files:
-		os.system('sbatch %s' % sbf)		
+		if jobnums is not None:
+			jobnum = int(sbf.split('.sh')[0].split('sbatch')[1])
+			if jobnum in jobnums:
+				os.system('sbatch %s' % sbf)		
+		else:
+				os.system('sbatch %s' % sbf)		
+			
+# Change a parameter given by key to value in jobs given by jobnums. If jobnums is none, the change
+# will be applied to all jobs in directory. Flag sbatch tells whether we should look for parameter
+# key in the job_param file or the sbatch file (for example to change the job clock time, we would
+# set sbatch = True)
+def change_param(key, value, directory, sbatch = False, jobnums = None):
+
+	# Grab all files
+	if sbatch:
+		files = glob.glob('uoicorr/%s/sbatch*' % directory)
+
+		for file in files:
+			if jobnums is not None:
+				jobnum = int(file.split('.sh')[0].split('sbatch')[1])
+
+				if jobnum in jobnums:
+					with open(file, 'r+') as f:
+					# change parameter and add sbatch to list
+						edit_sbatch(f, key, value)	
+
+			else:
+				with open(file, 'r+') as f:
+				# change parameter and add sbatch to list
+					edit_sbatch(f, key, value)
+
+	else:
+		files = glob.glob('uoicorr/%s/*.json' % directory)
+
+
+		for file in files:
+			if jobnums is not None:
+				jobnum = int(file.split('.sh')[0].split('sbatch')[1])
+
+				if jobnum in jobnums:
+					with open(file, 'r+') as f:
+					# change parameter and add sbatch to list
+						params = json.load(f)
+						params[key] = value
+						f.close()
+			else:
+				with open(file, 'r+') as f:
+				# change parameter and add sbatch to list
+					params = json.load(f)
+					params[key] = value
+					f.close()
+
+# Given an sbatch file, find parameter given by flag and set to value
+def edit_sbatch(fobject, flag, value):
+	fcontent = fobject.read()
+	paramstring = '#SBATCH %s' % flag
+	start_loc = fcontent.find(paramstring)
+	if start_loc != -1:
+		end_loc = start_loc + len(paramstring) + 1
+
+		endofline = fcontent.find('\n', end_loc)
+
+		current_value = fcontent[end_loc:endofline]
+		fcontent = fcontent.replace(current_value, value)
+
+		fobject.write(fcontent)
+	else:
+		print('Could not find flag in sbatch file')
 
