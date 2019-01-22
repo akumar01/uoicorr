1d840a0ac6b825a3203726dc479fcb9a28afab52
akumar01 <akumar.4db@gmail.com>
2019-01-21 23:53:54 -0800

    Selection details analysis and fixing postprocess once and for all

--- postprocess.py
+++ postprocess.py
@@ -1,10 +1,11 @@
-import os, glob
+import os, glob, sys
 import json
 import h5py
 import pickle
 import numpy as np
 import pandas as pd
 import itertools
+import importlib
 import pdb
 
 
@@ -19,7 +20,7 @@ def postprocess(data_file, params):
 	# fn, fp, r2 are in shape (reps, correlations)
 
 	data_list = []
-	
+
 	for covidx, cov_param in enumerate(params['cov_params']):
 		data_dict = {} 
 
@@ -32,6 +33,7 @@ def postprocess(data_file, params):
 			data_dict['betas'] = data_file['betas'][:]
 		except:
 			data_dict['betas'] = data_file['beta'][:]
+
 
 		data_dict['r2_true'] = data_file['r2_true'][:, covidx]
 		data_dict['r2'] = data_file['r2'][:, covidx]
@@ -84,16 +86,26 @@ def postprocess_file(data_file, param_file):
 
 # Postprocess an entire directory of data, will assume standard nomenclature of
 # associated parameter files
-def postprocess_dir(dirname):
+# old_format: back when we were using the .py param files
+def postprocess_dir(dirname, old_format = False):
 	# Collect all .h5 files
 	data_files = glob.glob('%s/*.h5' % dirname)
 	# List to store all data
 	data_list = []
 
+	if old_format:
+		sys.path.append(dirname)
+
 	for data_file in data_files:
-		with open('%s_params.json' % data_file.split('.h5')[0], 'r') as f:
-			# Load the corresponding parameter file
-			params = json.load(f)
+		if old_format:
+			module_name = os.path.split(data_file)[1].split('.h5')[0]
+			params = importlib.import_module('%s_params' % module_name)
+			params = dict_from_module(params)
+			params['cov_params'] = params.pop('correlations')
+		else:
+			with open('%s_params.json' % data_file.split('.h5')[0], 'r') as f:
+				# Load the corresponding parameter file
+				params = json.load(f)
 		try:
 			# Load the data
 			file = h5py.File(data_file, 'r')
@@ -113,3 +125,12 @@ def postprocess_dir(dirname):
 	# pickle.dump(dataframe, f)
 	print(dataframe.shape)
 	return dataframe
+
+# util function to deal with legacy usage of .py param files
+def dict_from_module(module):
+    context = {}
+    for setting in dir(module):
+    	if not '__' in setting:
+	        context[setting] = getattr(module, setting)
+
+    return context
