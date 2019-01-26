a8d0ff0c9a547a2209860d80a8b3f76fdc773351
akumar01 <akumar.4db0@gmail.com>
2019-01-25 17:30:46 -0800

    Fixed uoicorr_imports once and for all

--- uoicorr_imports.py
+++ uoicorr_imports.py
@@ -11,24 +11,27 @@ import sys, os, pdb
 # Hack to import pyuoi
 parent_path, current_dir = os.path.split(os.path.abspath('.'))
 while current_dir not in ['nse']:
-	parent_path, current_dir = os.path.split(parent_path)
+    parent_path, current_dir = os.path.split(parent_path)
 p = os.path.join(parent_path, current_dir)
 # Add analysis
 if p not in sys.path:
-	sys.path.append(p)
+    sys.path.append(p)
 
 import subprocess
 hname = subprocess.check_output('hostname')
 
 if 'ankitnse'.encode() in hname:
 
-	if '%s/uoicorr' % p not in sys.path:
-	    sys.path.append('%s/uoicorr' % p)
+    if '%s/uoicorr' % p not in sys.path:
+        sys.path.append('%s/uoicorr' % p)
+    if '%s/PyUoI' % p not in sys.path:
+        sys.path.append('%s/PyUoI' % p)
+
 else:
-	# And standard list of subdirectories
-	if '%s\\pyuoi' % p not in sys.path:
-		sys.path.append('%s\\pyuoi' % p)
-	if '%s\\uoicorr' % p not in sys.path:
-	    sys.path.append('%s\\uoicorr' % p)
+    # And standard list of subdirectories
+    if '%s\\pyuoi' % p not in sys.path:
+        sys.path.append('%s\\pyuoi' % p)
+    if '%s\\uoicorr' % p not in sys.path:
+        sys.path.append('%s\\uoicorr' % p)
 
 from postprocess import postprocess_file, postprocess_dir
