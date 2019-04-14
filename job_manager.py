5f8e12e861aacd4521b460afd076bd8d12a6d847
akumar01 <akumar.4db0@gmail.com>
2019-04-13 21:24:40 -0700

    modification to job_manager

--- job_manager.py
+++ job_manager.py
@@ -101,11 +101,20 @@ def generate_sbatch_scripts(job_array, script_dir):
 
         script = 'mpi_submit.py'
 
+        # If UoI, parallelize across bootstraps
+        # Otherwise, take the max of 64 and the number of reps required
+        # by this particular job
+        if job['exp_type'] in ['UoIElasticNet', 'UoILasso']:
+            nprocs = 48
+        else:
+            nprocs = 64
+
         with open(sbname, 'w') as sb:
             # Arguments common across jobs
             sb.write('#!/bin/bash\n')
             sb.write('#SBATCH -q regular\n')
             sb.write('#SBATCH -N 1\n')
+
             sb.write('#SBATCH -t %s\n' % job['job_time'])
 
             sb.write('#SBATCH --job-name=%s\n' % jobname)
@@ -116,29 +125,17 @@ def generate_sbatch_scripts(job_array, script_dir):
 
             # Work with out own Anaconda environment
             # To make this work, we had to add some paths to our .bash_profile.ext
+            sb.write('source ~/anaconda3/bin/activate\n')
             sb.write('source activate nse\n')
-
+            
             # Critical to prevent threads competing for resources
-            sb.write('export OMP_NUM_THREADS=1')
-            sb.write('export MKL_NUM_THREADS=1')
-
-            # Specify architecture on Cori
-            sb.write('if [ "$NERSC_HOST = cori" ]; then\n')
-            sb.write('  #SBATCH -C haswell\n')       
-            sb.write('fi\n')
-
-            # If UoI, parallelize across bootstraps
-            # Otherwise, take the max of 64 and the number of reps required
-            # by this particular job
-            if job['exp_type'] in ['UoIElasticNet', 'UoILasso']:
-                nprocs = 48
-            else:
-                nprocs = 64
+            sb.write('export OMP_NUM_THREADS=1\n')
+            sb.write('export MKL_NUM_THREADS=1\n')
 
             sb.write('srun -n %d python3 -u %s/%s %s' 
                     % (nprocs, script_dir, script, job['arg_file']))
 
-def create_job_structure(arg_file, data_dir = 'uoicorr/dense'):
+def create_job_structure(arg_file, data_dir = 'uoicorr/dense', skip_arg_files=True):
 
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
@@ -208,7 +205,7 @@ def create_job_structure(arg_file, data_dir = 'uoicorr/dense'):
 #            generate_log_file(job_array_chunks[i][j], jobdir)
             job_array_chunks[i][j] = generate_arg_files(job_array_chunks[i][j], results_files, jobdir)
             generate_sbatch_scripts(job_array_chunks[i][j], script_dir)
-
+        
 
 #     # Initialize arrays that keep track of whether particular jobs have run and whether they have
 #     # succesfully completed            
@@ -220,21 +217,109 @@ def create_job_structure(arg_file, data_dir = 'uoicorr/dense'):
 #     with open('%s/log.dat' % data_dir, 'wb') as f:
 #         pickle.dump(f, [job_array_chunks, run_status, completion_status])
 
-def run_jobs(jobdir):
+# Jobdir: Directory to crawl through
+# size: only submit this many jobs (if exp_type specified, this
+# means only submit this many jobs of this exp_type(s))
+# exp_type: Only run these kinds of experiments
+# edit_attribute: Dict containing key value pair of job property to
+# edit before submitting
+# run: If set to false, make modifications to sbatch files but do not run them
+def run_jobs(jobdir, constraint, size = None, exp_type = None, run = True):
     
     # Crawl through all subdirectories and 
     # (1) change permissions of sbatch file
     # (2) run sbatch file
 
-    run_files = []
+    run_files = grab_sbatch_files(jobdir, exp_type)
+            
+    if size is not None:
+        run_files = run_files[:size]
+    cont = input("You are about to submit %d jobs, do you want to continue? [0/1]" % len(run_files))
+    
+    if cont:
+        for run_file in run_files:
+            # Need to open up the file and include the particular constraint
+            # On Edison - remove any constraints
+            # On Cori - can have either KNL or Haswell
+            f = open(run_file, 'r')
+            contents = f.readlines()
+            f.close()
 
-    for root, dirs, files in os.walk(jobdir):
+            constraint_string = [s for s in contents if '--constraint' in s]
+
+            # First remove any existing constraint strings
+            if len(constraint_string) > 0:
+                for c in constraint_string:
+                    contents.remove(c)
+            
+            # Add constraint to the top
+            if constraint == 'haswell':
+                contents.insert(1, '#SBATCH --constraint=haswell\n')
+              
+            if constraint == 'knl':
+                contents.insert(1, '#SBATCH --constraint=knl\n')                  
+            
+            f = open(run_file, "w")
+            contents = "".join(contents)
+            f.write(contents)
+            f.close()
+            
+            if run:
+                # Change permissions
+                os.system('chmod u+x %s' % run_file)
+                # Submit the job
+                os.system('sbatch %s ' % run_file)
+
+# Edit specific lines of all sbatch files 
+# By default, edits an attribute
+# To replace a specific line with exact match to string, set edit_attribute
+# to None, and pass in linestring instead
+def edit_job_attribute(jobdir, edit_attribute, linestring = None, exp_type = None):
+    
+    run_files = grab_sbatch_files(jobdir, exp_type)
+
+    for run_file in run_files:
+        start = time.time()
+        f = open(run_file, 'r')
+        contents = f.readlines()
+        f.close()
+
+        if edit_attribute is not None:
+            for key, value in edit_attribute.items():
+                key_string = [s for s in contents if ' %s' % key in s][0]
+                key_string_idx = contents.index(key_string)
+                if '--' in key:
+                    new_key_string = '#SBATCH %s=%s\n' % (key, value)
+                else:
+                    new_key_string = '#SBATCH %s %s\n' % (key, value)
+                contents.remove(key_string)
+                contents.insert(key_string_idx, new_key_string)
+        elif linestring is not None:
+            for key, value in linestring.items():
+                key_string = [s for s in contents if key in s][0]
+                key_string_idx = contents.index(key_string)
+                contents.remove(key_string)
+                contents.insert(key_string_idx, value)
+
+        f = open(run_file, "w")
+        contents = "".join(contents)
+        f.write(contents)
+        f.close()
+        print('Iteration time: %f' % (time.time() - start))
+        
+# Crawl through a subdirectory and grab all jobs contained in it:
+def grab_sbatch_files(root_dir, exp_type = None):
+    run_files = []
+    for root, dirs, files in os.walk(root_dir):
         for d in dirs:
             p = os.path.join(root, d)
-            run_files.extend(glob('%s/*.sh' % p))
-
-    print(run_files)
-
+            if exp_type is not None:
+                if exp_type in p:
+                    run_files.extend(glob('%s/*.sh' % p))
+            else:
+                run_files.extend(glob('%s/*.sh' % p))
+    return run_files
+    
 # Upon running this command, check which jobs have been successfully completed, and 
 # submit another batch of jobs to be run
 def evaluate_job_structure(root_dir):
