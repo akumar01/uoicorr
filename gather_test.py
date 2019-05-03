188cd2013d78855adf18b84549115f1baf30e9f0
akumar01 <akumar.4db0@gmail.com>
2019-05-03 10:36:51 -0700

    testing gather across subcomm

--- gather_test.py
+++ gather_test.py
@@ -1,41 +1,56 @@
 import sys, os
 from mpi4py import MPI
 import numpy as np
-parent_path, current_dir = os.path.split(os.path.abspath('.'))
-
-
-
-##################### (just to handle my particular path structure) #####################
-# Crawl up to the repos folder
-while current_dir not in ['repos', 'nse']:
-    print('In loop!')
-    parent_path, current_dir = os.path.split(parent_path)
-
-p = os.path.join(parent_path, current_dir)
-
-# Add uoicorr and pyuoi to the path
-if '%s/uoicor' % p not in sys.path:
-    sys.path.append('%s/uoicorr' % p)
-if '%s/PyUoI' % p not in sys.path:
-    sys.path.append('%s/PyUoI' % p)
-
 from pyuoi.mpi_utils import Gatherv_rows
-
-########################################################################################
+from utils import gen_beta2, gen_data
+from sklearn.metrics import r2_score
 
 # Initialize comm object
 comm = MPI.COMM_WORLD
 rank = comm.rank
 numproc = comm.Get_size()
 
-# Generate some random data
-data = np.random.random_integers(0, 100, size = (np.random.randint(1, 10), 1000))
-#data = np.random.random_integers(0, 100, size = (10, 1000))
-print('Rank %d has size (%d, %d)' % (rank, data.shape[0], data.shape[1]))
+ranks = np.arange(numproc)
+split_ranks = np.array_split(ranks, 20)
+color = [i for i in np.arange(20) if rank in split_ranks[i]][0]
+subcomm_roots = [split_ranks[i][0] for i in np.arange(20)]
 
-print('Starting gather')
+subcomm = comm.Split(color, rank)
 
-# Gather data
-data = Gatherv_rows(data, comm, root = 0)
+rank = color
+nchunks = 20
+subrank = subcomm.rank
+numproc = subcomm.Get_size()
 
-print('Data gathered!')
+# Create a group including the root of each subcomm. 
+global_group = comm.Get_group()
+root_group = MPI.Group.Incl(global_group, subcomm_roots)
+roots_comm = comm.Create(root_group)
+
+if subrank == 0: 
+	r2_scores = np.zeros(5)
+
+for i in range(5)
+    if subrank == 0:
+        # Generate data
+        beta = gen_beta2()
+        X, X_test, y, y_test = gen_data(180, beta = beta)
+    else:
+        X = None
+        X_test = None
+        y = None 
+        y_test = None
+        
+    X = Bcast_from_root(X, subcomm)
+    X_test = Bcast_from_root(X, subcomm)
+    y = Bcast_from_root(y, subcomm)
+    y_test = Bcast_from_root(y_test, subcomm)
+
+    if subrank == 0:
+    	r2_scores[i] = r2_score(y_test, X_test @ beta)
+
+if subrank == 0:
+	print('Starting gather')
+	r2_scores = Gatherv_rows(r2_scores, roots_comm, root = 0)
+	print (r2_scores)
+	
