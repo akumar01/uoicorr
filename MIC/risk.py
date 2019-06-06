ca0dcb72d7227ead0f8040e8c388f92f9f866a34
akumar01 <akumar.4db@gmail.com>
2019-06-06 09:53:16 -0700

    added risk.py, notebook

--- MIC/risk.py
+++ MIC/risk.py
@@ -9,6 +9,17 @@ def calc_KL_div(mu_hat, sigma_hat, sigma):
 
 	n = mu_hat.size
 
-	exact_KL_div = n/2 * (np.log(sigma_hat**2) + sigma**2/(sigma_hat**2)) + 1/(2 * sigma_hat**2) * np.linalg.norm(mu_hat)**2
+	exact_KL_div = \
+	1/2 * (n * np.log(sigma_hat**2)/np.sqrt(2 * np.pi * sigma**2) + \
+		   n * sigma**2/(sigma_hat**2) + 1/(sigma_hat**2) * np.linalg.norm(mu_hat)**2)
 
 	return exact_KL_div
+
+# Calculate the AIC estimate of the KL div term above
+def AIC(y_true, y_pred, n_features):
+
+	n = y_true.size
+
+	AIC =1/2 * (np.log(2 * np.pi/n * np.linalg.norm(y_true - y_pred)**2) + 1) + n_features 
+
+	return AIC
