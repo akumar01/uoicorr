ed9c93dec207ebe1d4c10f44d0a55a3d8d41c0f1
akumar01 <akumar.4db0@gmail.com>
2019-07-17 15:48:02 -0700

    working on UoISelector

--- exp_types.py
+++ exp_types.py
@@ -1,32 +1,36 @@
 import numpy as np
 import pdb
+import itertools
+import time
+
 from sklearn.linear_model.coordinate_descent import _alpha_grid
 from sklearn.linear_model import LassoCV, ElasticNetCV
 from sklearn.model_selection import KFold, GroupKFold, cross_validate
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import normalize
+
 from pyuoi.linear_model.lasso import UoI_Lasso
 from pyuoi.linear_model.cassolasso import PycassoLasso
 from pyuoi.linear_model.elasticnet import UoI_ElasticNet
-from gtv import GraphTotalVariance
-import itertools
-import time
 from pyuoi.mpi_utils import Gatherv_rows
 from pyuoi.lbfgs import fmin_lbfgs
 
-class Selector():
+from gtv import GraphTotalVariance
+from info_criteria import GIC, eBIC
+from utils import selection_accuracy
 
-    def __init__(self, selection_method = 'CV'): 
+class UoISelector(Selector):
 
-        self.selection_method = selection_method
+    def __init__(self, selection_method = 'CV'):
 
-    def select(self, *args):
+        super(UoISelector, self).__init__(selection_method)
 
-        # Deal to the appropriate sub-function based on 
-        # the provided selection method string
+    def select(self, *args): 
+
+        # For UoI, interpret CV selector to mean r2
         if self.selection_method == 'CV':
-            self.CV_selector(*args)
-        # Straightforward scoring functions
+            self.r2_selector(*args)
+
         elif self.selection_method in ['BIC', 'AIC']: 
             self.GIC_selector(*args)
         elif self.selection_method == 'mBIC':
@@ -38,9 +42,37 @@ class Selector():
         elif self.selection_method == 'aBIC':
             self.aBIC_selector(*args)
 
-        self.coefs = solutions[self.selection_idx, :]
-        self.reg_param = reg_params[self.selection_idx]
+    def r2_selector(self, X, y, solutions, reg_params, true_model)
 
+        # UoI Estimates have shape (n_boots_est, n_supports, n_coef)
+
+        n_boots, n_supports, n_coefs = solutions.shape
+        scores = np.zeros((n_boots, n_supports))
+        for boot in n_boots:
+            scores[boot, :] = 
+
+class Selector():
+
+    def __init__(self, selection_method = 'CV'): 
+
+        self.selection_method = selection_method
+
+    def select(self, *args):
+        # Deal to the appropriate sub-function based on 
+        # the provided selection method string
+        if self.selection_method == 'CV':
+            self.CV_selector(*args)
+
+        elif self.selection_method in ['BIC', 'AIC']: 
+            self.GIC_selector(*args)
+        elif self.selection_method == 'mBIC':
+            self.mBIC_selector(*args)
+        elif self.selection_method == 'eBIC':
+            self.eBIC_selector(*args)
+        elif self.selection_method == 'OIC':
+            self.OIC_selector(*args)
+        elif self.selection_method == 'aBIC':
+            self.aBIC_selector(*args)
 
     # Assumes the output of cross_validate
     def CV_selector(self, test_scores, estimators, 
@@ -53,18 +85,20 @@ class Selector():
         n_reg_params = len(reg_params)
 
         if self.selection_method == 'BIC':
-            penalty = np.log(samples)
+            penalty = np.log(n_samples)
         if self.selection_method == 'AIC':
             penalty = 2
         
         # Should be of the shape n_reg_params by n_samples
         y_pred = solutions @ X.T
-
-        scores = np.array([GIC(y.ravel(), y_pred[i, :], np.count_nonzero(solutions[i, :],
-                                penalty)) for i in range(n_reg_params)])
+        scores = np.array([GIC(y.ravel(), y_pred[i, :], np.count_nonzero(solutions[i, :]),
+                                penalty) for i in range(n_reg_params)])
 
         # Select the coefficients and reg_params with lowest score
-        self.selection_idx = int(np.argmin(self.scores))
+        self.selection_idx = np.argmin(scores)
+        self.coefs = solutions[self.selection_idx, :]
+        self.reg_param = reg_params[self.selection_idx]
+
 
     def mBIC_selector(self, X, y, solutions, reg_params, true_model):
 
@@ -83,7 +117,9 @@ class Selector():
                                 for i in range(n_reg_params)])
 
         # Select the coefficients and reg_params with lowest score
-        self.selection_idx = int(np.argmin(self.scores))
+        self.selection_idx = int(np.argmin(scores))
+        self.coefs = solutions[self.selection_idx, :]
+        self.reg_param = reg_params[self.selection_idx]
 
 
     def OIC_selector(self, X, y, solutions, reg_params, true_model): 
@@ -99,19 +135,21 @@ class Selector():
         # Fix the penalty to be between 0 and 4 times the BIC
         penalties = np.linspace(0, 4 * np.log(n_samples), 50)
         selection_accuracies = np.zeros(penalties.size)
-        selection_idxs = np.zeros(penalties.size)
+        selection_idxs = np.zeros(penalties.size, dtype = np.int32)
         for i, penalty in enumerate(penalties):
 
             scores = np.array([GIC(y.ravel(), y_pred[j, :], 
-                               np.count_nonzero(solutions[j, :], penalty)) 
+                               np.count_nonzero(solutions[j, :]), penalty) 
                                for j in range(n_reg_params)])
 
-            selection_idxs[i] = np.argmin(self.scores)
+            selection_idxs[i] = np.argmin(scores)
             selection_accuracies[i] = selection_accuracy(true_model, 
-                                      solutions[selection_idx, :])
+                                      solutions[selection_idxs[i], :])
 
         self.oracle_penalty = penalties[np.argmax(selection_accuracies)]
         self.selection_idx = selection_idxs[np.argmax(selection_accuracies)]
+        self.coefs = solutions[self.selection_idx, :]
+        self.reg_param = reg_params[self.selection_idx]
 
     def aBIC_selector(self, X, y, solutions, reg_params, true_model):
 
@@ -176,35 +214,44 @@ class UoILasso():
         else:
             comm = None
 
-        if 'forward_selection' in list(args.keys()):
-            forward_selection = args['forward_selection']
-        else:
-            forward_selection = False
+        # Container for results
+        self.results = {selection_method : {} 
+                        for selection_method in selection_methods}
 
-        if 'manual_penalty' in list(args.keys()):
-            manual_penalty = args['manual_penalty']
-        else:
-            manual_penalty = 2
+        for selection_method in selection_methods:
+            self.fit_and_select(args, comm, X, y.ravel(), selection_method)
 
-        if 'ss' in list(args.keys()):
-            noise_level = args['ss']
-        else:
-            noise_level = 0
+        return self.results
 
-        uoi = UoI_Lasso(
-            normalize=False,
-            n_boots_sel=int(args['n_boots_sel']),
-            n_boots_est=int(args['n_boots_est']),
-            estimation_score=args['est_score'],
-            stability_selection = args['stability_selection'],
-            manual_penalty = manual_penalty,
-            noise_level = noise_level,
-            comm = comm
-            )
+    @classmethod 
+    def fit_and_select(self, args, comm, 
+                       X, y, selection_method):
 
-        uoi.fit(X, y.ravel())
+        if not hasattr(self, 'fitted_estimator'):
+            # If not yet fitted, run the pycasso lasso
+            uoi = UoI_Lasso(
+                normalize=False,
+                n_boots_sel=int(args['n_boots_sel']),
+                n_boots_est=int(args['n_boots_est']),
+                estimation_score=args['est_score'],
+                stability_selection = args['stability_selection'],
+                comm = comm
+                )
 
-        return uoi
+            uoi.fit(X, y.ravel())
+            self.fitted_estimator = uoi
+
+        # Use the fact that UoI stores all of its estimates to
+        # manually go in and select models and then take the union
+        # using each distinct estimation score
+
+        if comm.rank == 0:
+            true_model = args['beta'].ravel()
+            selector = UoISelector(selection_method = selection_method)
+            selector.select(X, y, self.fitted_estimator, true_model)
+            self.results[selection_method]['coefs'] = selector.coefs
+            self.results[selection_method]['reg_param'] = selector.reg_param            
+            # Make sure to store the oracle penalty somewhere
 
 class UoIElasticNet():
 
@@ -257,104 +304,3 @@ class EN():
                         l1_ratio = l1_ratios).fit(X, y.ravel())
 
         return en
-
-class GTV():
-
-    @classmethod
-    def run(self, X, y, args, groups = None):
-        print('started run')
-        cv_splits = 5
-        lambda_S = np.linspace(0, 1, 10)
-        lambda_TV = np.linspace(0, 1, 10)
-        lambda_1 = np.linspace(0, 1, 10)
-
-        cov = args['sigma']
-
-        if not isinstance(lambda_S, np.ndarray):
-            if np.isscalar(lambda_S):
-                lambda_S = np.array([lambda_S])
-            else:
-                lambda_S = np.array(lambda_S)
-
-        if not isinstance(lambda_TV, np.ndarray):
-            if np.isscalar(lambda_TV):
-                lambda_TV = np.array([lambda_TV])
-            else:
-                lambda_TV = np.array(lambda_TV)
-
-        if not isinstance(lambda_1, np.ndarray):
-            if np.isscalar(lambda_1):
-                lambda_1 = np.array([lambda_1])
-            else:
-                lambda_1 = np.array(lambda_1)
-
-        if 'comm' in list(args.keys()):
-            comm = args['comm']
-        else:
-            comm = None
-
-        if 'use_skeleton' in list(args.keys()):
-            use_skeleton = args['use_skeleton']
-        else:
-            use_skeleton = True
-
-        if 'threshold' in list(args.keys()):
-            threshold = args['threshold']
-        else:
-            threshold = False
-
-        scores = np.zeros((lambda_S.size, lambda_TV.size, lambda_1.size))
-
-#        Use k-fold cross_validation
-        kfold = KFold(n_splits = cv_splits, shuffle = True)
-
-        # Parallelize hyperparameter search
-        hparamlist = list(itertools.product(lambda_S, lambda_TV, lambda_1))
-
-        if comm is not None:
-            numproc = comm.Get_size()
-            rank = comm.rank
-            chunk_hparamlist = np.array_split(hparamlist, numproc)
-            chunk_idx = rank
-            num_tasks = len(chunk_hparamlist[chunk_idx])
-        else:
-            numproc = 1
-            rank = 0
-            chunk_hparamlist = [hparamlist]
-            chunk_idx = 0
-            num_tasks = len(hparamlist)
-
-        cv_scores = np.zeros(len(chunk_hparamlist[chunk_idx]))
-        for i, hparam in enumerate(chunk_hparamlist[chunk_idx]):
-            t0 = time.time()
-            gtv = GraphTotalVariance(lambda_S = hparam[0], lambda_TV = hparam[1],
-                                     lambda_1 = hparam[2], normalize=True,
-                                     warm_start = False, use_skeleton = use_skeleton,
-                                     threshold = threshold, minimizer = 'lbfgs')
-            scores = np.zeros(cv_splits)
-            fold_idx = 0
-            for train_index, test_index in kfold.split(X):
-                # Fits
-                gtv.fit(X[train_index, :], y[train_index], cov)
-                # Score
-                scores[fold_idx] = r2_score(y[test_index], X[test_index] @ gtv.coef_)
-                fold_idx += 1
-            cv_scores[i] = np.mean(scores)
-            print('hparam time: %f' % (time.time() - t0))
-        # Gather scores across processes
-        if comm is not None:
-            cv_scores = Gatherv_rows(cv_scores, comm, root=0)
-        if rank == 0 or comm is None:
-            print('finished iterating')
-            best_idx = np.argmax(cv_scores)
-            best_hparam = hparamlist[best_idx]
-            # Return GTV fit the best hparam
-            model = GraphTotalVariance(lambda_S = best_hparam[0],
-                                     lambda_TV = best_hparam[1],
-                                     lambda_1 = best_hparam[2], normalize=True,
-                                     warm_start = False, use_skeleton = use_skeleton,
-                                     threshold = threshold, minimizer = 'lbfgs')
-            model.fit(X, y, cov)
-        else:
-            model = None
-        return model
---
+++ info_criteria.py
@@ -0,0 +1,94 @@
+import numpy as np
+from pyuoi.utils import log_likelihood_glm
+import scipy
+
+# Generalized information criterion with arbitrary penalty
+def GIC(y, y_pred, model_size, penalty):
+
+    ll = log_likelihood_glm('normal', y, y_pred)
+
+    return -2 * ll + penalty * model_size
+
+# Extended BIC (found in literature)
+def eBIC(y, y_pred, n_features, model_size):
+
+    n_samples = y.size
+
+    # Kappa is a hyperparameter tuning the strength of the effect (1 -> no effect)
+    kappa = 0.5
+
+    eBIC = GIC(y, y_pred, model_size, np.log(n_samples)) + \
+    n_features *  2 * (1 - kappa) * np.log(float(scipy.special.binom(n_features, model_size)))
+
+    return eBIC
+
+# modified BIC penalty with some prior on the model size
+def mBIC(y, y_pred, model_size, sparsity_prior):
+
+    # make sure sparsity prior is epsilon less than 1
+    if sparsity_prior == 1:
+        sparsity_prior = 0.9999
+
+    mBIC =  BIC(y, y_pred, model_size) + 2 * model_size * np.log(1/sparsity_prior - 1)
+
+    return mBIC
+
+# Full Bayesian model penalty selection
+def bayesian_lambda_selection(y, y_pred, n_features, model_size, sparsity_prior, penalty):
+
+    y = y.ravel()
+    y_pred = y_pred.ravel()
+
+    n_samples = y.size
+
+    # Log likelihood
+    ll = log_likelihood_glm('normal', y, y_pred)
+
+    # Regularization Penalty (prior)
+    p1 = 2 * penalty * model_size
+
+    # Normal BIC penalty
+    BIC = model_size * np.log(n_samples)
+
+    # Second order Bayes factor approximation
+    RSS = np.sum((y - y_pred)**2)
+    BIC2 = n_samples**3/(2 * RSS*3)
+
+    # Term arising from normalization
+    BIC3 = model_size * np.log(2 * np.pi)
+
+    # If provided with a list of sparsity estimates, we are specifying
+    # a beta hyperprior, and need to integrate over it correspondingly
+    if not np.isscalar(sparsity_prior):
+        M_k = beta_binomial_model(sparsity_prior, n_features, model_size)
+    else:
+        if sparsity_prior == 1:
+            sparsity_prior = 0.999
+
+        # Model probability prior
+        M_k = scipy.special.binom(n_features, model_size) * \
+              sparsity_prior**model_size * (1 - sparsity_prior)**(n_features - model_size)
+
+    P_M = 2 * np.log(M_k)
+
+#    bayes_factor = 2 * ll - BIC - BIC2 + BIC3 - p1 + P_M
+
+    return ll, p1, BIC, BIC2, BIC3, M_k, P_M
+
+
+# Return a posterior estimate for the binomial parameter given a 
+# beta-distribution prior on estimates of that parameter 
+def beta_binomial_model(x, n, k):
+
+    # drop all entries that are 0 
+    x = x[x != 0]
+
+
+
+    # Fit the parameters of the beta distribution
+    a, b, _, _ = scipy.stats.beta.fit(x, floc = 0, fscale = 1)
+
+    p = scipy.special.binom(n, k) * \
+            scipy.special.beta(k + a, n - k + b)/scipy.special.beta(a, b)
+    
+    return p
