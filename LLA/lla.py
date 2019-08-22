b2c519fc3fc6c019540a5ed1ce64d162d759149d
akumar01 <akumar.4db0@gmail.com>
2019-08-22 13:40:27 -0700

    finalized implementation of lla.py

--- LLA/lla.py
+++ LLA/lla.py
@@ -11,21 +11,23 @@ import pdb
 # Custom implementation of Fast iterative thresholding algorithm
 def FISTA(x0, A, b, alpha, max_iter = 1000, tol=1e-6):
 
-    # Minimize ||A x - b||^2 + \alpha |x|_1, where alpha can be a vector of diff.
+    # Minimize 1/(2n) * ||A x - b||^2 + \alpha |x|_1, where alpha can be a vector of diff.
     # values
+
+    n = b.size
 
     # Thresholding operator: alpha is a vector of the same size as x that allows
     # one to separately set the thresholding for each entry of x
     T_lambda = lambda alpha, x: np.multiply(np.sign(x), np.maximum(np.abs(x) - alpha, 0))
     # Iterative shrinkage operator for L1 regularization
-    p_L = lambda y, A, b, alpha, t: T_lambda(alpha * t, y - 2 * t * A.T @ (A @ y - b))    
+    p_L = lambda y, A, b, alpha, t: T_lambda(alpha * t, y - 1/b.size * t * A.T @ (A @ y - b))    
 
     # Used to assess stopping criteria
-    loss_fn = lambda y, A, b, alpha: np.linalg.norm(A @ y - b)**2 + alpha @ np.abs(y)
+    loss_fn = lambda y, A, b, alpha: 1/(2 * b.size) * np.linalg.norm(A @ y - b)**2 + alpha @ np.abs(y)
     
     # Calculate the Lipshitz constant associated with this problem 
     # Calculate the largest eigenvalue of the Hessian
-    L = 2 * eigh(A.T @ A, eigvals_only=True, eigvals=(A.shape[1] - 1, A.shape[1] - 1))
+    L = 2 * eigh(1/n * A.T @ A, eigvals_only=True, eigvals=(A.shape[1] - 1, A.shape[1] - 1))
 
     # Main FISTA loop
     y = x0
@@ -40,15 +42,9 @@ def FISTA(x0, A, b, alpha, max_iter = 1000, tol=1e-6):
 
     for k in range(max_iter):
         x[1] = p_L(y, A, b, alpha, 1/L)
-        # FISTA should always give decreasing values
         loss[k] = loss_fn(x[1], A, b, alpha)
-        try:
-            if k >= 1:
-                assert(loss[k] <= loss[k-1])
-        except:
-            continue
 
-        # Stopping criteria: Difference in function values should be less than        
+        # Stopping criteria: Difference in function values should be less than tol        
         if k > 1:
             if np.abs(loss[k] - loss[k-1]) < tol:
                 break
@@ -64,7 +60,7 @@ def FISTA(x0, A, b, alpha, max_iter = 1000, tol=1e-6):
     final_loss = loss_fn(x[1], A, b, alpha)
     final_tol = np.abs(final_loss - loss_fn(x[0], A, b, alpha))
 
-    return (x[1], final_loss, final_tol)
+    return (x[1], final_loss, final_tol, k)
 
 
 def LLA(beta_0, X, y, penalty_fn, penalty_fn_args=(), max_iter=100, tol=1e-6):
@@ -85,8 +81,6 @@ def LLA(beta_0, X, y, penalty_fn, penalty_fn_args=(), max_iter=100, tol=1e-6):
         # Use FISTA to solve the weighted L1 optimization
         beta[1] = FISTA(beta[0], X, y, w)[0]
 
-        pdb.set_trace()
-
         # Stopping criteria:
         if np.linalg.norm(beta[1] - beta[0]) < tol:
             break
@@ -97,7 +91,7 @@ def LLA(beta_0, X, y, penalty_fn, penalty_fn_args=(), max_iter=100, tol=1e-6):
         # Shift indices
         beta[0] = beta[1]
 
-    return beta
+    return (beta[1], m)
 
 # Derivative of SCAD penalty
 def dSCAD(x, a, alpha):
